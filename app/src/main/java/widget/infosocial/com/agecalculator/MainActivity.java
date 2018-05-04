978ef206128e3af098549b770ecd06ef30b5f64d
Jonathan Cuesto <34cuesto@gmail.com>
2018-05-04 14:34:07 -0400

    Logo added

--- app/src/main/java/widget/infosocial/com/agecalculator/MainActivity.java
+++ app/src/main/java/widget/infosocial/com/agecalculator/MainActivity.java
@@ -1,20 +1,30 @@
 package widget.infosocial.com.agecalculator;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnStart).setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, AgeCalculatorActivity.class  );
-            startActivity(i);
-        });
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, AgeCalculatorActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURACION_SPLASH);
 
     }
 }
